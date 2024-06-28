65ea3edf70eef19f945fa677286cc50f637a59b0
Piyush903 <piyushnijhawan78@gmail.com>
2024-06-28 19:28:10 +0530

    changes

---
+++ routes/speechRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const multer = require('multer');
+const { createSpeech, getSpeeches, updateSpeech, deleteSpeech,getSpeechById } = require('../controllers/speechController');
+const router = express.Router();
+
+const upload = multer();
+
+router.post('/', upload.single('file'), createSpeech);
+router.get('/', getSpeeches);
+router.put('/:id',upload.array() ,updateSpeech);
+router.delete('/:id', deleteSpeech);
+router.get('/:id', getSpeechById);
+module.exports = router;
